0158698a28accd54390559af9a14790140ad03f2
jimmy <quesoo@gmail.com>
2020-06-04 12:58:24 +0200

    jason: add overlay-pa

--- device.mk
+++ device.mk
@@ -33,7 +33,8 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-pa
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
