fed32947acd8a71b4b2a9f27413ccb89409943c8
jimmy <quesoo@gmail.com>
2020-10-15 20:30:01 +0200

    jason: adapt NFC for android 11

--- device.mk
+++ device.mk
@@ -126,7 +126,9 @@ PRODUCT_PACKAGES += \
     nqnfcse_access.xml \
     Tag \
     com.android.nfc_extras \
-    vendor.nxp.hardware.nfc@1.2-service \
+    vendor.nxp.hardware.nfc@2.0-service \
+    vendor.nxp.nxpnfclegacy@1.0.vendor \
+    se_nq_extn_client \
     nfc_nci.nqx.default.hw
 
 PRODUCT_SOONG_NAMESPACES += vendor/nxp/opensource/pn5xx
