66a0aa529fc864a5fc6434c9fa3e180b00b30c55
darkarrowgit <yigitemirhandurmus@hotmail.com>
2020-12-01 17:59:07 +0000

    jason: nuke lineage stuff aosp goes brr

--- device.mk
+++ device.mk
@@ -131,10 +131,6 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
-# Lineage hardware
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.jason
-
 # NFC
 PRODUCT_PACKAGES += \
     NfcNci \
