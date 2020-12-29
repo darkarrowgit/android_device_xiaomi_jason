471aaa6c1631b5fe73e126ff048ac87cb61b9ed3
jimmy <quesoo@gmail.com>
2020-12-29 20:59:08 +0100

    jason: selinux permissive
    Change-Id: I78741f314a1bf471926ea1bc3f4c1780801c3d10

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,6 +29,7 @@ DEVICE_PATH := device/xiaomi/jason
 # Kernel
 TARGET_KERNEL_CONFIG := jason_defconfig
 TARGET_KERNEL_VERSION := 4.4
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # Camera
 BOARD_QTI_CAMERA_32BIT_ONLY := true
